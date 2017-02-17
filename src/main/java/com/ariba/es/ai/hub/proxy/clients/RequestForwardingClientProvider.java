820c7732a0bc5e6bc348f2ada5e1c5bf5664d366
sanjay dc <sanjay.dc@sap.com>
2017-02-17 10:16:55 +0530

    selenium grid extra docker extension:

---
+++ src/main/java/com/ariba/es/ai/hub/proxy/clients/RequestForwardingClientProvider.java
@@ -0,0 +1,11 @@
+package com.ariba.es.ai.hub.proxy.clients;
+
+/**
+ * @author Alexey Nikolaenko alexey@tcherezov.com
+ *         Date: 22/09/2015
+ */
+public class RequestForwardingClientProvider {
+    public RequestForwardingClient provide(String host, int port, String api, String queryParameters) {
+        return new RequestForwardingClient(host, port, api, queryParameters);
+    }
+}
